c36cf317f4dcdc081df933293fac7824cca677d3
Georgie Reis <georgie.reis@farmaciaindiana.far.br>
2022-05-05 18:44:58 -0300

    Revert "Criação de Token JWT"
    This reverts commit 0e94cbb8f0721e4acb0363d121369e6a64deb5de.

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $connection = 'sgep_equipe';
-
     /**
      * The attributes that are mass assignable.
      *
