a6cf6bea77a620febadabd4cd68c6159dbd93567
Georgie Reis <georgie.reis@farmaciaindiana.far.br>
2022-05-05 18:44:39 -0300

    Revert "Update User.php"
    This reverts commit fc13b02442f07e0e4cffe51967b67a18ff3e7313.

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $connection = 'sgep_equipe';
+
     /**
      * The attributes that are mass assignable.
      *
